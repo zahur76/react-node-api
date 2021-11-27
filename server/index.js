9dfd150586ef1a3e02e27e1fe7f412656c8b5d44
zahur <zahurmeerun@hotmail.com>
2021-11-27 10:24:44 +0400

    add react frontend app

--- server/index.js
+++ server/index.js
@@ -8,8 +8,8 @@ const app = express();
 
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
-});
-
+  });
+  
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
