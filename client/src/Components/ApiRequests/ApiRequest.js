c302aebbd46f60571ad1441c85c16026e50ece74
zahur <zahurmeerun@hotmail.com>
2021-11-27 17:48:16 +0400

    add accordion to todo list

--- client/src/Components/ApiRequests/ApiRequest.js
+++ client/src/Components/ApiRequests/ApiRequest.js
@@ -1,5 +1,9 @@
 import React from "react";
 import './ApiRequests.css';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button';
+import Accordion from 'react-bootstrap/Accordion'
 
 function ApiRequests() {
     const [data, setData] = React.useState(null);    
@@ -7,16 +11,44 @@ function ApiRequests() {
     React.useEffect(() => {fetch("/api").then((res) => res.json())
         .then((data) => setData(data));
     }, []) 
+
+    const myFunction=(event)=>{
+        console.log(event.target.value)
+    }
     
-    const listItems = (data || []).map((number) =>
-        <div>
-            <div>{number.id}</div>
-            <div>{number.name}</div>
+    const listItems = (data || []).map((element) =>
+        <div className="text-center">
+            <Accordion defaultActiveKey="1">
+                <Accordion.Item eventKey="0">
+                        <Accordion.Header>
+                            <Col xs={12}>
+                                <div className="text-dark font-italic">{element.id}.{element.name}</div>
+                            </Col>                                                                                                         
+                        </Accordion.Header>
+                    <Accordion.Body>
+                        <Row className="text-center todo-list">
+                            <Col xs={4}>Created</Col>
+                            <Col xs={4}>Completion</Col>
+                            <Col xs={4} md={2}>Status</Col>                            
+                            <Col className="border-bottom border-dark mb-2" xs={12}></Col>
+                            <Col xs={4}>{element.date_created}</Col>
+                            <Col xs={4}>{element.date_completion}</Col>
+                            <Col xs={4} md={2}>{element.completed}</Col>
+                            <Col className="buttons mt-2" xs={12}>                                
+                                <Button onClick={myFunction} value={element.id} className="m-1 bg-danger text-light text-right">Remove</Button>
+                                <Button className="m-1 bg-success text-light text-right">Update</Button>                                
+                            </Col>                                                                                   
+                        </Row>
+                    </Accordion.Body>
+                </Accordion.Item>
+            </Accordion>            
         </div>
-    );
-    
+    );    
     return (
-        <div className="requests">        
+        <div className="requests">
+            <Row className="m-0">
+                <a href="#" className="btn bg-info text-success col-12 mt-3 mb-3 w-75 mx-auto">Add Item</a>
+            </Row>                   
             {listItems}
         </div>
     );
