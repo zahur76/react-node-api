9adf7a98fbcc57369b6591fd36ef431ed8ddc353
zahur <zahurmeerun@hotmail.com>
2021-11-28 19:52:52 +0400

    update static file location

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser')
 const app = express();
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, './client/build')));
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 //environment variables
 const dotenv = require('dotenv');
